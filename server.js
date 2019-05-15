833eeb54c7daabed68d9434a46ece1a4c35afdbc
aldinh777 <aldi.nh789@gmail.com>
2019-05-15 15:01:19 +0800

    Remove Session

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const path = require('path')
-const session = require('express-session')
 
 const routes = require('./routes')
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(routes)
-app.use(session({secret: 'ninja', saveUninitialized: false, resave: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 const server = app.listen(process.env.PORT || 3000, _ => {
